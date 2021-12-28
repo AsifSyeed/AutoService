e786457614eaf1cdc05df11cb691a904a971420e
AsifSyeed <Asif.Syeed@brainstation-23.com>
2021-12-29 00:28:18 +0600

    Add role repository
    Signed-off-by: AsifSyeed <Asif.Syeed@brainstation-23.com>

---
+++ src/main/java/com/example/autoservice/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.autoservice.repository;
+
+import com.example.autoservice.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
